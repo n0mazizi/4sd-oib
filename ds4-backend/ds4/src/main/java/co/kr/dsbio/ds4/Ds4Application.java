2e55c35dbd54c3eab6a6fe439c73f5c1191b8759
n0mazizi <heoyunhoe@gmail.com>
2023-02-09 13:44:12 +0900

    springboot starter 등록

---
+++ ds4-backend/ds4/src/main/java/co/kr/dsbio/ds4/Ds4Application.java
@@ -0,0 +1,13 @@
+package co.kr.dsbio.ds4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ds4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ds4Application.class, args);
+	}
+
+}
---
+++ ds4-backend/ds4/src/test/java/co/kr/dsbio/ds4/Ds4ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.dsbio.ds4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ds4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
